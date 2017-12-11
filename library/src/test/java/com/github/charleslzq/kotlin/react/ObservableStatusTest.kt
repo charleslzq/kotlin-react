0b5f8b3240752b2a665a523f9c6c228b98e288cc
Eric Charles <github@ericcharleslzq.com>
2017-12-11 11:51:11 +0800

    ObservableStatus Unit Test

---
+++ library/src/test/java/com/github/charleslzq/kotlin/react/ObservableStatusTest.kt
@@ -0,0 +1,50 @@
+package com.github.charleslzq.kotlin.react
+
+import com.github.charleslzq.kotlin.react.ObservableStatus.Companion.getDelegate
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by charleslzq on 17-12-11.
+ */
+class ObservableStatusTest {
+    private var data by ObservableStatus(0)
+    private val oldData = 0
+
+    @Before
+    fun reset() {
+        data = oldData
+    }
+
+    @Test
+    fun testSubscriberWork() {
+        val newData = 4
+        var oldValue = -1
+        var newValue = -1
+        getDelegate(this::data)!!.onChange {
+            oldValue = it.first
+            newValue = it.second
+        }
+
+        data = newData
+
+        assertThat("data change received", oldValue.to(newValue), `is`(oldData to newData))
+    }
+
+    @Test
+    fun testMultiSubscriber() {
+        var count = 0
+        getDelegate(this::data)!!.onChange {
+            count++
+        }
+        getDelegate(this::data)!!.onChange {
+            count++
+        }
+
+        data = 4
+
+        assertThat("data change received twice", count, `is`(2))
+    }
+}
