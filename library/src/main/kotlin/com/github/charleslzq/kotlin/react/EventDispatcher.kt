8312e675e4a8815cad04911538e9e3195a0d4a95
Eric Charles <github@ericcharleslzq.com>
2017-12-21 18:07:09 +0800

    improve the way to compose middleware

--- library/src/main/kotlin/com/github/charleslzq/kotlin/react/EventDispatcher.kt
+++ library/src/main/kotlin/com/github/charleslzq/kotlin/react/EventDispatcher.kt
@@ -16,28 +16,17 @@ object EventDispatcher {
                         it(store, dispatch, next)
                     }
                 }
-                (compose(chain.subList(0, chain.size - 1), chain.last()))(dispatch)
+                chain.reduceRight { function, composed ->
+                    { next: (Any) -> Unit ->
+                        function(composed(next))
+                    }
+                }(dispatch)
             }
         }
     }
 
     fun buildMiddleWare(handler: MWApi.() -> ((Any) -> Unit)): (Any, (Any) -> Unit, (Any) -> Unit) -> ((Any) -> Unit) = { store, dispatch, next ->
         with(MWApi(store, dispatch, next), handler)
-    }
-
-    private tailrec fun compose(head: List<((Any) -> Unit) -> ((Any) -> Unit)>, tail: ((Any) -> Unit) -> ((Any) -> Unit)): ((Any) -> Unit) -> ((Any) -> Unit) {
-        return when (head.size) {
-            0 -> tail
-            1 -> { next: (Any) -> Unit ->
-                head[0](tail(next))
-            }
-            else -> {
-                val newTail = { next: (Any) -> Unit ->
-                    head.last()(tail(next))
-                }
-                compose(head.slice(0..(head.size - 2)), newTail)
-            }
-        }
     }
 
     class MWApi(val store: Any, val dispatch: (Any) -> Unit, val next: (Any) -> Unit)
